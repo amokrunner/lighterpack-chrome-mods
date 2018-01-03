481ec80075c1f8a0ccd8daa19032e49542654193
Danny Murphy <danny.murphy@amokrunner.com>
2018-01-03 07:48:24 -0800

    switching to userscript

--- lighterpack-chrome-mods.js
+++ lighterpack-chrome-mods.js
@@ -1,6 +1,16 @@
-$(document).ready(function () {
-    
-    // LIBRARY ACTIVE FILTER:
+// ==UserScript==
+// @name         Active Filter, Image Height Fix
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  Upgrades for lighterpack!
+// @author       amokrunner
+// @match        https://lighterpack.com/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
     $('#librarySearch').before('<div><input type="checkbox" id="librarySearchFilter" name="librarySearchFilter" /><label for="librarySearchFilter">Include Active Items</label></div>');
     $('#library').addClass('lpHideActive');
     $("#librarySearchFilter").change(function(){
@@ -13,15 +23,8 @@ $(document).ready(function () {
     $('head').append(`
       <style>
         .lpHideActive .lpLibraryItem:not(.lpItemNotInList){ display:none !important;}
-      </style>
-    `);
-    
-    // IMAGE HEIGHT LIMIT:
-    $('head').append(`
-      <style>
         .lpImageCell{ text-align: center !important; }
         .lpItemImage { max-height: 4em !important; }
       </style>
     `);
-    
-});
+})();
